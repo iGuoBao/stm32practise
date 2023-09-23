fead569ada8680f29103da16119196c76303919e
Jin Zihao <b2567336804@gmail.com>
2023-09-23 18:52:16 +0800

    Revert "GPIO输入简易测试完成"
    This reverts commit 89e583fd2b333905bbd1114d38b6ea1d153e5b88.

--- 练习项目/user/main.c
+++ 练习项目/user/main.c
@@ -1,32 +1,14 @@
-#include "stm32f10x.h"    // Device header
-
-// 公共
-#include "setSysClk.h"
-
-// 用户外设
+#include "stm32f10x.h"                  // Device header
 #include "LED.h"
-#include "Button.h"
-
-
-
-
+#include "setSysClk.h"
 
 int main(void)
 {
-<<<<<<< Updated upstream
 	setSysClkHSE();		// 设置HSE 为时钟源
 	LEDinit();				// LED0 1 初始化 默认亮
 	JLEDinit();				// JLED1-8 初始化 默认亮
-=======
-	set_SysClkHSE();		// 设置HSE 为时钟源
-	LED_init();					// LED0 1 初始化 默认亮
-	JLED_init();				// JLED1-8 初始化 默认亮
->>>>>>> Stashed changes
 	while(1)
 	{
-		if(IsButtonPressed(KEY0_PORT,KEY0_PIN))
-		{
-			GPIO_WriteBit(LED0_PORT, LED0_PIN, !GPIO_ReadOutputDataBit(LED0_PORT, LED0_PIN)); // 电平取反
-		}
+		
 	}
 }
