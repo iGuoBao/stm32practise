578f9172cec9b7d550bd2481a69bc66002c5e106
Jin Zihao <b2567336804@gmail.com>
2023-09-24 17:34:28 +0800

    Revert "中断完成  待优化"
    This reverts commit 0cac6f8aca64d2ec901e98ec64be52c168ad3aa6.

--- 练习项目/user/Button/Button.c
+++ 练习项目/user/Button/Button.c
@@ -8,14 +8,14 @@ void KEY_init(void)
 	// 定义一个GPIO初始化结构体变量
   GPIO_InitTypeDef GPIO_InitStructure; 
 	// 使能三个端口的时钟
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE); 
+  RCC_APB2PeriphClockCmd(KEY0_PORT_RCC|KEY1_PORT_RCC|KEY2_PORT_RCC, ENABLE); 
   
   // 配置KEY0的GPIO参数
   GPIO_InitStructure.GPIO_Pin = KEY0_PIN; 							// 设置引脚
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 			  // 设置为上拉输入模式
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 	  // 设置速度为50MHz
   GPIO_Init(KEY0_PORT, &GPIO_InitStructure); 					  // 初始化GPIOA
-	// GPIO_WriteBit(KEY0_PORT, KEY0_PIN,1);									  // 上拉 则需要输出高电平
+	GPIO_WriteBit(KEY0_PORT, KEY0_PIN,1);									  // 上拉 则需要输出高电平
   
   // 配置KEY1的GPIO参数，与KEY0类似
   GPIO_InitStructure.GPIO_Pin = KEY1_PIN;
